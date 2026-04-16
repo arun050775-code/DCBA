d2c664aecfcb279f075625582abc888af82027d3
arun050775-code <arun050775@gmail.com>
2026-04-16 11:15:48 +0530

    Add files via upload

---
+++ src/supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://xalbjrmridjgdpguobdx.supabase.co'
+const supabaseKey = 'sb_publishable_LuLFWWbmK8i9NQ8BV3SIZA__6Usbvfs'
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
